bb23bc902fac38175fde0ef701de113860aee051
Matthieu <matthieu.mauny@gmail.com>
2021-04-13 14:47:23 +0200

    express - path params

--- digi-express/server.js
+++ digi-express/server.js
@@ -7,8 +7,17 @@ const todos = [
      { description: "Expliquer les bases d'express !", done: true}
     ]
 
-app.get('/', (req, rep) => {
+app.get('/todos', (req, rep) => {
     rep.json(todos)
+})
+
+app.get('/todos/:status', (req, rep) => {
+    const status = req.params.status;
+    rep.json(todos.filter(todo => todo[status]))
+})
+
+app.post('/todos', (req, rep) => {
+    rep.send('requete post !')
 })
 
 app.listen(8000, () => console.log('On écoute sur le port 8000 !'))
