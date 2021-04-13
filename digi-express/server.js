5c8cdb7694ff2695c96bad156330854fea45a5ce
Matthieu <matthieu.mauny@gmail.com>
2021-04-13 14:20:29 +0200

    express - json

--- digi-express/server.js
+++ digi-express/server.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const todos = [
+     { description: "Faire les courses", done: false},
+     { description: "Expliquer les bases d'express !", done: true}
+    ]
+
 app.get('/', (req, rep) => {
-    rep.send('La racine')
+    rep.json(todos)
 })
 
 app.listen(8000, () => console.log('On écoute sur le port 8000 !'))
